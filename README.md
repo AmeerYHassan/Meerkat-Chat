a719d90ef43792970bf9ee8e3f06d894a9891ea7
EC2 Default User <ec2-user@ip-172-31-64-222.ec2.internal>
2020-10-10 20:46:33 +0000

    skeleton code

---
+++ README.md
@@ -0,0 +1,2 @@
+# Chat App
+## Ameer Hassan
---
+++ app.py
@@ -0,0 +1,54 @@
+# app.py
+from os.path import join, dirname
+from dotenv import load_dotenv
+import os
+import flask
+import flask_sqlalchemy
+
+app = flask.Flask(__name__)
+
+
+dotenv_path = join(dirname(__file__), 'sql.env')
+load_dotenv(dotenv_path)
+
+sql_user = os.environ['SQL_USER']
+sql_pwd = os.environ['SQL_PASSWORD']
+dbuser = os.environ['USER']
+
+database_uri = 'postgresql://{}:{}@localhost/postgres'.format(
+    sql_user, sql_pwd)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
+
+db = flask_sqlalchemy.SQLAlchemy(app)
+db.app = app
+
+import models 
+
+@app.route('/')
+def index():
+    models.db.create_all()
+    addresses = [
+        models.Usps("1600 Pennsylvania"),
+        models.Usps("121 W 21st Ave"),
+        models.Usps("NJIT GITC")]
+    for address in addresses:
+        db.session.add(address)
+    db.session.commit()
+    
+    retstr = "<html>"
+    dbAddresses = models.Usps.query.all()
+    retstr += "<ol>"
+    for dbAddress in dbAddresses:
+        retstr += "<li>" + dbAddress.address + "</li>"
+    retstr += "</ol>"
+    retstr += "</html>"
+    return retstr
+    
+    
+if __name__ == '__main__':
+    app.run(
+        host=os.getenv('IP', '0.0.0.0'),
+        port=int(os.getenv('PORT', 8080)),
+        debug=True
+    )
