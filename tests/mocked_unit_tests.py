188cf0438dcebf1ac8fb314017ae93c546c2d1c1
EC2 Default User <ec2-user@ip-172-31-64-222.ec2.internal>
2020-10-26 22:53:15 +0000

    linting finished for tests

--- tests/mocked_unit_tests.py
+++ tests/mocked_unit_tests.py
@@ -1,9 +1,14 @@
+# pylint: disable=C0116, C0115, C0114, W0613, R0903, E0401, C0413
+# C0116, C0114, C0115 disabled since docstrings are not necessary to understand functions here
+# W0613 disabled because link is mandatory to be passed in, even though it is not used.
+# R0903 disabled because not many class methods are needed
+# E0401, C0413 state that app is not being imported, but it is being imported correctly.
+
 from unittest import mock
 import unittest
 import sys
 
 from os.path import dirname, join
-import sys
 sys.path.insert(1, join(dirname(__file__), '../'))
 
 import app
@@ -12,37 +17,44 @@ class MockedResponse:
     def __init__(self, status_code, json):
         self.status_code = status_code
         self.json_dict = json
-        
+
     def json(self):
         return self.json_dict
 
 def return_valid_cat_fact(link):
-    currCat = MockedResponse(200, {"fact":"Cats have four legs!"})
-    return currCat
+    curr_cat = MockedResponse(200, {"fact":"Cats have four legs!"})
+    return curr_cat
 
 def return_invalid_cat_fact(link):
-    currCat = MockedResponse(404, {"fact":"blabla"})
-    return currCat
+    curr_cat = MockedResponse(404, {"fact":"blabla"})
+    return curr_cat
 
 def return_valid_funtranslate(link):
-    currTranslation = MockedResponse(200, {'contents': {'translated': 'A test to trying to get output for me to make a test case,  Force be with you this is'}})
-    return currTranslation
+    curr_translation = MockedResponse(200,
+        {'contents':
+            {'translated': \
+                'A test to trying to get output for me to make a test case,  '\
+                'Force be with you this is'}
+        })
+    return curr_translation
 
 def return_valid_funtranslate_with_special_characters(link):
-    currTranslation = MockedResponse(200, {'contents': {'translated': 'filler text'}})
-    return currTranslation
+    curr_translation = MockedResponse(200, {'contents': {'translated': 'filler text'}})
+    return curr_translation
 
 def return_invalid_funtranslate(link):
-    currTranslation = MockedResponse(404, {'contents': {'translated': 'filler text'}})
-    return currTranslation
-    
+    curr_translation = MockedResponse(404, {'contents': {'translated': 'filler text'}})
+    return curr_translation
+
 def return_valid_gif(link):
-    currGif = MockedResponse(200, {'data': [{'images': {'original': {'url': 'https://mock.url/'}}}]})
-    return currGif
+    curr_gif = MockedResponse(200,
+        {'data': [{'images': {'original': {'url': 'https://mock.url/'}}}]})
+    return curr_gif
 
 def return_invalid_gif(link):
-    currGif = MockedResponse(404, {'data': [{'images': {'original': {'url': 'https://mock.url/'}}}]})
-    return currGif
+    curr_gif = MockedResponse(404,
+        {'data': [{'images': {'original': {'url': 'https://mock.url/'}}}]})
+    return curr_gif
 
 class ChatbotTestCase(unittest.TestCase):
     def test_valid_cat_fact(self):
@@ -50,55 +62,59 @@ class ChatbotTestCase(unittest.TestCase):
             response = app.getBotResponse({}, "!!catfact")["message"]
             expected = "Cats have four legs!"
             self.assertEqual(response, expected)
-    
+
     def test_invalid_cat_fact(self):
         with mock.patch('app.requests.get', return_invalid_cat_fact):
             response = app.getBotResponse({}, "!!catfact")["message"]
             expected = "Uh oh! Try again in a few, I can't seem to get a cat fact right now."
             self.assertEqual(response, expected)
-    
+
     def test_valid_translation(self):
         with mock.patch('app.requests.get', return_valid_funtranslate):
             response = app.getBotResponse({}, "!!funtranslate filler")["message"]
-            expected = "A test to trying to get output for me to make a test case,  Force be with you this is"
+            expected = \
+                "A test to trying to get output for me to make a test case,"\
+                "  Force be with you this is"
             self.assertEqual(response, expected)
-    
+
     def test_invalid_translation(self):
         with mock.patch('app.requests.get', return_invalid_funtranslate):
             response = app.getBotResponse({}, "!!funtranslate filler")["message"]
             expected = "Uh oh! Try again in a few, I can't seem to translate that right now."
             self.assertEqual(response, expected)
-    
+
     def test_valid_translation_with_specials(self):
         with mock.patch('app.requests.get', return_valid_funtranslate_with_special_characters):
             response = app.getBotResponse({}, "!!funtranslate this isn't correct!")["message"]
             expected = "Sorry, I only can translate sentences that just contain letters! Try again"
             self.assertEqual(response, expected)
-    
+
     def test_valid_gif(self):
         with mock.patch('app.requests.get', return_valid_gif):
             response = app.getBotResponse({}, "!!giphy test")["imageLink"]
             expected = 'https://mock.url/'
             self.assertEqual(response, expected)
-    
+
     def test_invalid_gif(self):
         with mock.patch('app.requests.get', return_invalid_gif):
             response = app.getBotResponse({}, "!!giphy test")["message"]
             expected = "Uh oh! Try again in a few, I can't seem to get a gif right now."
             self.assertEqual(response, expected)
-    
+
     def test_no_gif_returned(self):
         with mock.patch('app.requests.get', return_valid_gif):
             response = app.getBotResponse({}, "!!giphy test")["imageLink"]
             expected = "Uh oh! Try again in a few, I can't seem to get a gif right now."
             self.assertNotEqual(response, expected)
-    
+
     def test_valid_translation_with_specials_false(self):
         with mock.patch('app.requests.get', return_valid_funtranslate_with_special_characters):
             response = app.getBotResponse({}, "!!funtranslate this isn't correct!")["message"]
-            expected = "A test to trying to get output for me to make a test case,  Force be with you this is"
+            expected = \
+                "A test to trying to get output for me to make a test case, "\
+                " Force be with you this is"
             self.assertNotEqual(response, expected)
-    
+
     def test_valid_cat_fact_false(self):
         with mock.patch('app.requests.get', return_valid_cat_fact):
             response = app.getBotResponse({}, "!!catfact")["message"]
--- tests/unmocked_unit_tests.py
+++ tests/unmocked_unit_tests.py
@@ -1,3 +1,8 @@
+# pylint: disable=C0114, E0401, C0413, C0115, C0116
+# C0114, C0116 disabled because docstrings are not needed to understand tests here.
+# E0401, C0413 state app is not being imported, but it is.
+
+
 from datetime import datetime
 import unittest
 import sys
@@ -10,13 +15,16 @@ class ChatbotTestCase(unittest.TestCase):
         self.success_test_params = [
             {
                 "KEY_INPUT": "!!help",
-                "KEY_EXPECTED": "!!funtranslate <message> to translate a message to yoda speech, !!time to get the current time, "\
-                                "!!catfact to get a random cat fact, !!giphy <query> to retrieve a gif, "\
+                "KEY_EXPECTED": "!!funtranslate <message> to translate a message to yoda speech, "\
+                                "!!time to get the current time, "\
+                                "!!catfact to get a random cat fact, "\
+                                "!!giphy <query> to retrieve a gif, "\
                                 "or !!about to learn a bit more about me"
             },
             {
                 "KEY_INPUT": "!!about",
-                "KEY_EXPECTED": "Hi, I'm meerkat, and I'm just a small utility for this chat room! Use !!help to see what I can do!"
+                "KEY_EXPECTED": "Hi, I'm meerkat, and I'm just a small utility "\
+                                "for this chat room! Use !!help to see what I can do!"
             },
             {
                 "KEY_INPUT": "!!AAAAAA",
@@ -24,37 +32,46 @@ class ChatbotTestCase(unittest.TestCase):
             },
             {
                 "KEY_INPUT": "!about",
-                "KEY_EXPECTED": "Hi, I'm meerkat, and I'm just a small utility for this chat room! Use !!help to see what I can do!"
+                "KEY_EXPECTED": "Hi, I'm meerkat, and I'm just a small utility "\
+                                "for this chat room! Use !!help to see what I can do!"
             },
             {
                 "KEY_INPUT": "!!time",
                 "KEY_EXPECTED": "The current time is " + datetime.now().strftime("%H:%M:%S")
             },
             {
-                "KEY_INPUT": "!!funtranslate hello, this is a test to trying to get output for me to make a test case",
-                "KEY_EXPECTED": "Sorry, I only can translate sentences that just contain letters! Try again"
+                "KEY_INPUT": "!!funtranslate hello, this is a test "\
+                             "to trying to get output for me to make a test case",
+                "KEY_EXPECTED": "Sorry, I only can translate sentences "\
+                                "that just contain letters! Try again"
             }
         ]
-        
+
         self.failure_test_params = [
             {
                 "KEY_INPUT": "!!about",
-                "KEY_EXPECTED": "!!funtranslate <message> to translate a message to yoda speech, !!time to get the current time, "\
-                                "!!catfact to get a random cat fact, !!giphy <query> to retrieve a gif, "\
+                "KEY_EXPECTED": "!!funtranslate <message> to translate a message to yoda speech, "\
+                                "!!time to get the current time, "\
+                                "!!catfact to get a random cat fact, "\
+                                "!!giphy <query> to retrieve a gif, "\
                                 "or !!about to learn a bit more about me"
             },
             {
                 "KEY_INPUT": "!!help",
-                "KEY_EXPECTED": "Hi, I'm meerkat, and I'm just a small utility for this chat room! Use !!help to see what I can do!"
+                "KEY_EXPECTED": "Hi, I'm meerkat, and I'm just a small utility for this chat room!"\
+                                "Use !!help to see what I can do!"
             },
             {
                 "KEY_INPUT": "!!abot",
-                "KEY_EXPECTED": "Hi, I'm meerkat, and I'm just a small utility for this chat room! Use !!help to see what I can do!"
+                "KEY_EXPECTED": "Hi, I'm meerkat, and I'm just a small utility for this chat room!"\
+                                "Use !!help to see what I can do!"
             },
             {
                 "KEY_INPUT": "!!h",
-                "KEY_EXPECTED": "!!funtranslate <message> to translate a message to yoda speech, !!time to get the current time, "\
-                                "!!catfact to get a random cat fact, !!giphy <query> to retrieve a gif, "\
+                "KEY_EXPECTED": "!!funtranslate <message> to translate a message to yoda speech, "\
+                                "!!time to get the current time, "\
+                                "!!catfact to get a random cat fact, "\
+                                "!!giphy <query> to retrieve a gif, "\
                                 "or !!about to learn a bit more about me"
             }
         ]
@@ -64,15 +81,13 @@ class ChatbotTestCase(unittest.TestCase):
         for test in self.success_test_params:
             response = app.getBotResponse({}, test["KEY_INPUT"])["message"]
             expected = test["KEY_EXPECTED"]
-
             self.assertEqual(response, expected)
 
     def test_parse_message_failure(self):
         for test in self.failure_test_params:
             response = app.getBotResponse({}, test["KEY_INPUT"])["message"]
             expected = test["KEY_EXPECTED"]
-            
             self.assertNotEqual(response, expected)
-            
+
 if __name__ == '__main__':
     unittest.main()
